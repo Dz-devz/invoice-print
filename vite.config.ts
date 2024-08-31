e7e3c6df95a918ccd39e6ddd1218d9a04c535c28
Dz.dev <darwinbjordan@gmail.com>
2024-08-31 14:59:27 +0800

    setup: added shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 // vite.config.ts
-import { defineConfig } from 'vite'
-import viteReact from '@vitejs/plugin-react'
-import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import viteReact from "@vitejs/plugin-react";
+import path from "path";
+import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +11,9 @@ export default defineConfig({
     viteReact(),
     // ...,
   ],
-})
-
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});

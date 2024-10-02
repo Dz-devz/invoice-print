80385200a0fe6a60892bf133eaca6e870cfa79cd
Dz-devz <darwinbjordan@gmail.com>
2024-10-02 20:28:54 +0800

    chore: improve ui for nav links

--- client/src/routes/__root.tsx
+++ client/src/routes/__root.tsx
@@ -3,24 +3,27 @@ import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="p-2 flex gap-2">
-        <Link to="/" className="[&.active]:font-bold">
-          Home
-        </Link>
-        <span> | </span>
-        <Link to="/about" className="[&.active]:font-bold">
-          About
-        </Link>
-        <span> | </span>
-        <Link to="/invoicesPage" className="[&.active]:font-bold">
-          Invoices
-        </Link>
-        <span> | </span>
-        <Link to="/createInvoice" className="[&.active]:font-bold">Create Invoice</Link>
+      <div className="p-4 bg-slate-50">
+        <div className="container mx-auto flex gap-6 justify-center">
+          <Link to="/" className="hover:text-green-500 text-gray-700 font-medium transition-all duration-200 ease-in-out [&.active]:font-bold">
+            Home
+          </Link>
+          <Link to="/about" className="hover:text-green-500 text-gray-700 font-medium transition-all duration-200 ease-in-out [&.active]:font-bold">
+            About
+          </Link>
+          <Link to="/invoicesPage" className="hover:text-green-500 text-gray-700 font-medium transition-all duration-200 ease-in-out [&.active]:font-bold">
+            Invoices
+          </Link>
+          <Link to="/createInvoice" className="hover:text-green-500 text-gray-700 font-medium transition-all duration-200 ease-in-out [&.active]:font-bold">
+            Create Invoice
+          </Link>
+        </div>
       </div>
-      <hr /> 
-      <Outlet />
+      <hr className="border-gray-300" />
+      <div className="container mx-auto mt-4 p-4">
+        <Outlet />
+      </div>
       {/* <TanStackRouterDevtools /> */}
     </>
   ),
-})
+});

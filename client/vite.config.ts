3a1e3efc1915f555aa3deaa931f926d2159beb3a
Dz.dev <darwinbjordan@gmail.com>
2024-12-07 23:00:02 +0800

    setup: set up viteconfig proxy for server

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,6 +1,7 @@
 // vite.config.ts
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import path from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
@@ -10,4 +11,21 @@ export default defineConfig({
     viteReact(),
     // ...,
   ],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+      "@server": path.resolve(__dirname, "../server"),
+    },
+  },
+  server: {
+    host: "0.0.0.0",
+    port: 4173,
+    proxy: {
+      "/api": {
+        target: "http://localhost:8080",
+        changeOrigin: true,
+      },
+    },
+  },
 });
